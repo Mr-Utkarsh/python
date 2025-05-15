7f2f136db053f61d94070b407703b6090beeeba1
Utkarsh Nageshwar <utkarshnageshwar@gmail.com>
2025-05-15 22:43:58 +0530

    u

---
+++ function.py
@@ -0,0 +1,123 @@
+# Function:-
+# 1.Inbuilt Function
+# 2.user-define functionDeclaration Syntax:-
+# def fun-name(parameter):
+#     used variables are declared as parameter
+#     =>perform operation
+#     =>return
+# function calling
+# fun-name(arguments(args are values against declare parameter))
+
+
+# required:-
+# =>def(keyword)
+# =>fun-name
+# =>()
+# =>:
+# =>body
+
+# optional:-
+# =>parameter
+# =>arguments
+# =>return(keyword)
+
+
+
+# def hello():
+#     pass
+# hello()
+# print(hello())
+
+# def hello():
+#     return None
+# hello()
+# print(hello())
+
+# def hello():
+#     return 'hello'
+# hello()
+# print(hello())
+
+# def hello():
+#     return
+# hello()
+# print(hello())
+
+# def hello():
+#     return ''
+# hello()
+# print(hello())
+
+# def hello():
+#     print("hello")
+# hello()
+
+# def hello():
+#     print("hello")
+# x=hello()
+# print(x)
+# print(x)
+# print(x)
+
+# def hello():
+#     return "hello"
+# x=hello()
+# print(x)
+# print(x)
+# print(x)
+
+# def natural_no(n): 
+#     for i in range(1,n+1): 
+#         print(i)
+# x=int(input("Enter any value = "))
+# natural_no(x)
+
+# def natural_no(n): 
+#     for i in range(1,n+1): 
+#         if i%2==0: 
+#             print(i)
+# x=int(input("Enter any value = "))
+# natural_no(x)
+
+# def natural_no(n): 
+#     for i in range(1,n+1): 
+#         if i%2!=0: 
+#             print(i)
+# x=int(input("Enter any value = "))
+# natural_no(x)
+
+# def natural_no(n): 
+#     sum=0
+#     for i in range(1,n+1): 
+#         if i%2!=0: 
+#             sum+=i
+#     print(sum)
+# x=int(input("Enter any value = "))
+# natural_no(x)
+
+# def natural_no(n): 
+#     sum=0
+#     for i in range(1,n+1): 
+#         if i%2==0: 
+#             sum+=i
+#     print(sum)
+# x=int(input("Enter any value = "))
+# natural_no(x)
+
+# def factorial(n): 
+#     fact=1
+#     for i in range(1,n+1): 
+#         if i>0: 
+#             fact*=i
+#     print(fact)
+# x=int(input("Enter any value = "))
+# factorial(x)
+
+# def factorial(n): 
+#     fact=1
+#     for i in range(1,n+1): 
+#         if i>0: 
+#             fact*=i
+#     print(fact)
+# x=int(input("Enter any value = "))
+# factorial(x)
