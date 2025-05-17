281ed7a3b3ffa266cfb3c0357eb3977a5b695aa6
Utkarsh Nageshwar <utkarshnageshwar@gmail.com>
2025-05-17 20:46:14 +0530

    update

--- hiordrfnctn.py
+++ hiordrfnctn.py
@@ -20,3 +20,19 @@
 # x=map(add5,l1,l2)
 # print(x)
 # print(tuple(x))
+
+# l1=[1,2,3,4,5]
+# l2=[5,4,3,2,1]
+# def add5(n,m): 
+#     return n//m
+# x=map(add5,l1,l2)
+# print(x)
+# print(tuple(x))
+
+# l1=[1,2,3,4,5]
+# l2=[5,4,3,2,1]
+# def add5(n,m): 
+#     return n-m
+# x=map(add5,l1,l2)
+# print(x)
+# print(tuple(x))
