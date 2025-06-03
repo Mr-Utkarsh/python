38ecfa5c9d7622fd4d89697c3e334bcf2bc061f3
Utkarsh Nageshwar <utkarshnageshwar@gmail.com>
2025-06-03 21:20:54 +0530

    u

--- problem_solving.py
+++ problem_solving.py
@@ -181,3 +181,183 @@ print("*"*i,
 # x=map(add5,l1,l2)
 # print(x)
 # print(tuple(x))
+
+#star pattern using while loop 
+
+# 1.  *****
+#     *****
+#     *****
+#     *****
+#     *****
+# n=int(input("Enter any value="))
+# i=1
+# while i<=n: 
+#     print('*'*n)
+#     i=i+1
+
+#2.*
+#  **
+#  ***
+#  ****
+#  *****
+# n=int(input("Enter any value="))
+# i=1
+# while i<=n: 
+#     print('*'*i)
+#     i=i+1
+
+#3.   *
+#    **
+#   ***
+#  ****
+# *****
+# n=int(input("Enter any value="))
+# i=1
+# while i<=n: 
+#     print(" "*(n-i)+"*"*i)
+#     i=i+1
+
+#4.    *
+#     * *
+#    * * *
+#   * * * *
+#  * * * * *
+# n=int(input("Enter any value="))
+# i=1
+# while i<=n: 
+#     print(" "*(n-i)+" *"*i)
+#     i=i+1
+
+#5.*****
+#  *****
+#   ****
+#    ***
+#     **
+#      *
+# n=int(input("Enter any value="))
+# i=0
+# while i<n: 
+#     print(" "*i+"*"*(n-i))
+#     i=i+1
+
+#6. * * * * *
+#    * * * *
+#     * * *
+#      * *
+#       *
+# n=int(input("Enter any value="))
+# i=0
+# while i<n: 
+#     print(" "*i+" *"*(n-i))
+#     i=i+1
+
+#7.*****
+#  ****
+#  ***
+#  **
+#  *
+# n=int(input("Enter any value="))
+# while n>0: 
+#     print("*"*n)
+#     n=n-1
+
+#8.
+# *****
+# **** 
+# ***  
+# **   
+# *    
+# **   
+# ***  
+# **** 
+# *****
+# n=int(input("Enter any value="))
+# i=0
+# while i<n: 
+#     print("*"*(n-i)+" "*i)
+#     i=i+1
+# # print(i)
+# i=i-2
+# while i>=0: 
+#     print("*"*(n-i)+" "*i)
+#     i=i-1
+
+#9.
+#  * * * * *
+#   * * * *
+#    * * *
+#     * *
+#      *
+#     * *
+#    * * *
+#   * * * *
+#  * * * * *
+# n=int(input("Enter any value="))
+# i=0
+# while i<n: 
+#     print(" "*i+" *"*(n-i))
+#     i=i+1
+# # print(i)
+# i=i-2
+# while i>=0: 
+#     print(" "*i+" *"*(n-i))
+#     i=i-1
+
+#10.
+#     *
+#    **
+#   ***
+#  ****
+#   ***
+#    **
+#     *
+# n=int(input("Enter any value="))
+# i=0
+# while i<n: 
+#     print(" "*(n-i)+"*"*i)
+#     i=i+1
+# # print(i)
+# i=i-2
+# while i>=0: 
+#     print(" "*(n-i)+"*"*i)
+#     i=i-1
+
+#11.
+#      *
+#     * *
+#    * * *
+#   * * * *
+#    * * *
+#     * *
+#      *
+# n=int(input("Enter any value="))
+# i=0
+# while i<n: 
+#     print(" "*(n-i)+" *"*i)
+#     i=i+1
+# # print(i)
+# i=i-2
+# while i>=0: 
+#     print(" "*(n-i)+" *"*i)
+#     i=i-1
+
+#12.
+# *****
+#  ****
+#   ***
+#    **
+#     *
+#    **
+#   ***
+#  ****
+# *****
+# n=int(input("Enter any value="))
+# i=0
+# while i<n: 
+#     print(" "*i+"*"*(n-i))
+#     i=i+1
+# # print(i)
+# i=i-2
+# while i>=0: 
+#     print(" "*i+"*"*(n-i))
+#     i=i-1 
