8eef3f0160c2a5a5f56ecacfe451deca72fba09c
Utkarsh Nageshwar <utkarshnageshwar@gmail.com>
2025-06-13 21:21:01 +0530

    u

---
+++ libraries/exercise1.py
@@ -0,0 +1,32 @@
+# Ex1 - Getting and Knowing your Data
+
+# Step 1. Import the necessary libraries
+
+'''import numpy as np
+import pandas as pd'''
+
+# Step 2. Import the dataset
+
+'''pd.read_csv("/Users/utkarshnageshwar/Desktop/code/pythonpandas/pandas_data.csv")'''
+
+# Step 3. Assign it to a variable called a.
+
+'''a=pd.read_csv("/Users/utkarshnageshwar/Desktop/code/pythonpandas/pandas_data.csv")
+a'''
+
+# Step 4. See the first 10 entries
+
+'''a.head(10)'''
+
+# Step 5. What is the number of observations in the dataset?
+
+'''a.shape[0]'''
+
+# Step 6. What is the number of columns in the dataset?
+
+'''a.shape[1]'''
+
+# Step 7. Print the name of all the columns.
+
+'''a.columns'''
+
